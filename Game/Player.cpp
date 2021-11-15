5d195439866cd978327b4a6a926570b3343c194b
fionaboulman <f.boulman@gmail.com>
2021-11-15 14:08:42 +0100

    Added Sprite and rendering

--- Game/Player.cpp
+++ Game/Player.cpp
@@ -1,40 +1,92 @@
 
 #include "Player.h"
-
+}
 void Player::initVariables()
 {
 	this->animState = PLAYER_ANIMATION_STATES::IDLE;
+}
+
+void Player::initTexture()
+{
+	if (!this->textureSheet.loadFromFile("Textures/cats.png"))
+	{
+		std::cout << "ERROR::PLAYER::Could not load the player sheet!" << "\n";
+	}
+}
+
+void Player::initSprite()
+{
+	this->sprite.setTexture(this->textureSheet);
+	this->currentFrame = sf::IntRect(0, 0, 64, 64); 
+
+	this->sprite.setTextureRect(this->currentFrame);
+	this->sprite.setScale(3.f, 3.f);
 }
 
 
 Player::Player()
 {
 	this->initVariables();
-}
+	this->initTexture();
+	this->initSprite();
 
 Player::~Player()
 {
 
 }
 
-
-const sf::Vector2f Player::getPosition() const
-{
-}
-
 const sf::FloatRect Player::getGlobalBounds() const
 {
+	return this->sprite.getGlobalBounds();
 }
 
 void Player::setPosition(const float x, const float y)
 {
+	this->sprite.setPosition(x, y);
+}
+
+
+void Player::updateMovement()
+{
+	this->animState = PLAYER_ANIMATION_STATES::IDLE;
+
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) 
+	{
+		this->move(-1.f, 0.f);
+		this->animState = PLAYER_ANIMATION_STATES::MOVING_LEFT;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) 
+	{
+		this->move(1.f, 0.f);
+		this->animState = PLAYER_ANIMATION_STATES::MOVING_RIGHT;
+	}
+		
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)) 
+	{
+		this->move(0.f, -1.f);
+		this->animState = PLAYER_ANIMATION_STATES::JUMPING;
+	}
+	else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S)) 
+	{
+		this->sprite.move(0.f, 1.f);
+	}
 }
 
 
 void Player::update()
 {	
+	this->updateMovement();
 }
 
 void Player::render(sf::RenderTarget & target)
 {
+	target.draw(this->sprite);
+
+	sf::CircleShape circ;
+	circ.setFillColor(sf::Color::Red);
+	circ.setRadius(2.f);
+	circ.setPosition(this->sprite.getPosition());
+
+	target.draw(circ);
+
 }
--- Game/Player.h
+++ Game/Player.h
@@ -5,18 +5,37 @@ enum PLAYER_ANIMATION_STATES {IDLE = 0, MOVING_LEFT, MOVING_RIGHT, JUMPING, FALL
 class Player
 {
 private:
+	sf::Sprite sprite;
+	sf::Texture textureSheet;
+	sf::Clock animationTimer;
+
+	//Core
 
 	void initVariables();
+	void initTexture();
+	void initSprite();
+
+	//bool isCollidingWithCoin(Coin* coin) {
+	//	if (this->getGlobalBounds().intersects(coin->getGlobalBounds())) {
+	//		return true;
+	//	}
+	//	return false;
+	//}
+
 
 public:
 	Player();
 	virtual ~Player();
 
+	//Accessors
 	const sf::Vector2f getPosition() const;
 	const sf::FloatRect getGlobalBounds() const;
 
+	//Modifiers
 	void setPosition(const float x, const float y);
 
+	//Functions	
+	void updateMovement();
 	void update();
 	void render(sf::RenderTarget& target);
 };
