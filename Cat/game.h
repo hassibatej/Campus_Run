d31a0f5a70a3e5f8418a5ce25cc90585841fb475
fionaboulman <f.boulman@gmail.com>
2021-12-06 13:39:58 +0100

    The Run base code + map

---
+++ Cat/game.h
@@ -0,0 +1,32 @@
+#pragma once
+#include"Player.h"
+
+class Game
+{
+private:
+	sf::RenderWindow window;
+	sf::Event ev;
+
+	Player* player;
+
+	void initWindow();
+	void initPlayer();
+
+public:
+
+	//Functions
+
+
+	Game();
+	virtual ~Game();
+
+	//Functions
+	void updatePlayer();
+	void updateCollision();
+	void update();
+	void renderPlayer();
+    void renderTileMap();
+	void render();
+	const sf::RenderWindow& getWindow() const;
+};
+
---
+++ Cat/sdtfx.h
@@ -0,0 +1,11 @@
+#ifndef SDTFX_H
+#define SDTFX_H
+
+
+class sdtfx
+{
+public:
+    sdtfx();
+};
+
+#endif // SDTFX_H
