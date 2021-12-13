9bb83480832c4fa5cac32b9cd7d60d3d3d35fa18
fionaboulman <f.boulman@gmail.com>
2021-12-13 14:33:27 +0100

    updated map + obstacles
    map is optimised by creation of the floor class + creation of obstacles

--- Cat/Tile.cpp
+++ Cat/Tile.cpp
@@ -11,7 +11,7 @@ Tile::~Tile()
 
 Tile::Tile(std::string textureName, float x, float y, bool passable, bool exit) {
     if (!setUpSprite(textureName)) {
-    return;
+    return ;
     }
     position = sf::Vector2f(x,y);
     sprite.setPosition(position);
@@ -19,42 +19,20 @@ Tile::Tile(std::string textureName, float x, float y, bool passable, bool exit) 
     isExit = exit;
 }
 
+/*int Tile::get_height(){
+    return height;
+}
+
+int Tile::get_length(){
+    return length;
+}*/
 bool Tile::setUpSprite(std::string textureName) {
     if (!texture.loadFromFile(textureName)) {
         return false;
     }
     texture.setSmooth(true);
     sprite.setTexture(texture);
-    sprite.setTextureRect(sf::IntRect(0,0,100,20));
+    sprite.setTextureRect(sf::IntRect(0,0, 100, 20));
     return true;
 }
 
-
-
-
-
-
-
-// cat code
-/*
-Tile::Tile(sf::Texture& texture_sheet, sf::IntRect texture_rect, bool damaging)
-	: damaging(damaging)
-{
-	this->sprite.setTexture(texture_sheet);
-	this->sprite.setTextureRect(texture_rect);
-}
-
-const sf::FloatRect Tile::getGlobalBounds() const
-{
-	return this->sprite.getGlobalBounds();
-}
-
-void Tile::update()
-{
-
-}
-
-void Tile::render(sf::RenderTarget& target)
-{
-	target.draw(this->sprite);
-*/
