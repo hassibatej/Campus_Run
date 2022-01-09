2a2adc1fa0c4758e9c070b1c79c1f045bcd2ad3f
antonina-mijatovic <antonina.mijatovic@gmail.com>
2022-01-09 23:06:06 +0100

    code done by antonina mijatovic

---
+++ final/SFML_RPG/TileMap.h
@@ -0,0 +1,79 @@
+#ifndef TILEMAP_H
+#define TILEMAP_H
+
+
+#include "RegularTile.h"
+#include "KillingTile.h"
+#include "LeftWall.h"
+#include "FlagTile.h"
+#include"Entity.h"
+#include"State.h"
+
+
+// Eve and Fiona worked on this file
+//antonina mijatovic worked on this file
+class TileMap
+{
+private:
+	void clear();
+
+	float gridSizeF;
+	int gridSizeI;
+	sf::Vector2i maxSizeWorldGrid;
+	sf::Vector2f maxSizeWorldF;
+	int layers;
+	std::vector< std::vector< std::vector< std::vector<Tile*> > > > map;
+	std::stack<Tile*> deferredRenderStack;
+	std::string textureFile;
+	sf::Texture tileSheet;
+	sf::RectangleShape collisionBox;
+
+	//Culling
+	int fromX;
+	int toX;
+	int fromY;
+	int toY;
+	int layer;
+
+	bool dead;
+	bool isflag;
+
+	TileMap(float gridSize, int width, int height, std::string texture_file);
+	TileMap(const std::string file_name);
+	virtual ~TileMap();
+
+	static TileMap* instance;
+
+public:
+	//antonina mijatovic worked on these two methods
+	static TileMap* getInstance();
+	static TileMap* recreateInstance();
+	//Accessors
+	const bool tileEmpty(const int x, const int y, const int z) const;
+	const sf::Texture* getTileSheet() const;
+	const int getLayerSize(const int x, const int y, const int layer) const;
+	const sf::Vector2i& getMaxSizeGrid() const;
+	const sf::Vector2f& getMaxSizeF() const;
+	//antonina mijatovic worked on these two functions
+	bool getDead() const;
+	bool getFlag() const;
+
+	//Functions
+	void loadFromFile(const std::string file_name);
+	const bool checkType(const int x, const int y, const int z, const int type) const;
+
+	void updateWorldBoundsCollision(Entity* entity, const float& dt);
+	void updateTileCollision(Entity* entity, const float& dt);
+	void update(Entity* entity, const float& dt);
+
+	void render(
+		sf::RenderTarget& target,
+		const sf::Vector2i& gridPosition,
+		sf::Shader* shader = NULL,
+		const sf::Vector2f playerPosition = sf::Vector2f(),
+		const bool show_collision = false
+	);
+	void renderDeferred(sf::RenderTarget& target, sf::Shader* shader = NULL, const sf::Vector2f playerPosition = sf::Vector2f());
+};
+
+#endif
---
+++ final/SFML_RPG/Won.h
@@ -0,0 +1,45 @@
+//Rojin Radmehr
+//antonina mijatovic worked on this file
+#pragma once
+
+#include "State.h"
+#include "Gui.h"
+#include "PauseMenu.h"
+#include "TileMap.h"
+#include "GameState.h"
+#include "SettingsState.h"
+#include"MainMenuState.h"
+#include"ComingSoon.h"
+
+
+class Won :
+	public State
+{
+private:
+	//Variables
+	sf::Texture backgroundTexture;
+	sf::RectangleShape background;
+	sf::Font font;
+
+	sf::RectangleShape btnBackground;
+	std::map<std::string, gui::Button*> buttons;
+
+	//Functions
+	void initVariables();
+	void initFonts();
+	void initKeybinds();
+	void initGui();
+	void resetGui();
+
+
+public:
+	Won(StateData* state_data);
+	virtual ~Won();
+
+	//Functions
+	void updateInput(const float& dt);
+	void updateButtons();
+	void update(const float& dt);
+	void renderButtons(sf::RenderTarget& target);
+	void render(sf::RenderTarget* target = NULL);
+};
